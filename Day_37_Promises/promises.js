73efefb3409aed9fff96387c7936790e96bc18dc
Ahsaan-ullah3236 <ahsan6377202@gmail.com>
2025-02-22 18:01:52 +0500

    Add descriptive paragraph to promises.html and remove unused code from promises.js

--- Day_37_Promises/promises.js
+++ Day_37_Promises/promises.js
@@ -5,12 +5,3 @@ const promiseOne = new Promise(function (resolve, reject ){
         
     }, 1000);
 });
-let a=3;
-let b=3; 
-let c=a+b;
-console.log(c);
-promiseOne.then(function () {
-    console.log("promise is resolved");
-}).catch(function () {
-    console.log("promise is rejected");
-});
